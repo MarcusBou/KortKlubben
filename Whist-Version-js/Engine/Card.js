5ea62e47e47afffc99cd77738a07e3ea6f870aa7
TGPGamez <58465104+TGPGamez@users.noreply.github.com>
2022-06-02 10:39:54 +0200

    Card and symbol

--- Whist-Version-js/Engine/Card.js
+++ Whist-Version-js/Engine/Card.js
@@ -1,3 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.Card = void 0;
 /**
  * Class for Card
  *
@@ -6,46 +9,37 @@
  * @version 1.01
  * @author Tobias
  */
-export class Card {
-    //The number on the card
-    #number;
-    //Which Symbol the card has
-    #symbol;
-
-    /**
-     * Get the number
-     * @return Card number
-     */
-    get getNumber() {
-        return this.#number;
-    }
-
-    /**
-     * Get the symbol
-     * @return Card symbol
-     */
-    get getSymbol() {
-        return this.#symbol;
-    }
-
-
+var Card = /** @class */ (function () {
     /**
      * Constructor
      * @param symbol Symbol of card
      * @param number Number of card
      */
-    constructor(symbol, number) {
-        this.#symbol = symbol;
-        this.#number = number;
+    function Card(symbol, number) {
+        this.symbol = symbol;
+        this.number = number;
     }
-
-    isSameSymbol(symbol) {
-        return this.#symbol == symbol;
-    }
-
-    isHigher(numb) {
-        return this.#number >= numb;
-    }
-
-    static Empty = new Card(null, 0);
-}
+    /**
+     * Get the number
+     * @return Card number
+     */
+    Card.prototype.GetNumber = function () {
+        return this.number;
+    };
+    /**
+     * Get the symbol
+     * @return Card symbol
+     */
+    Card.prototype.GetSymbol = function () {
+        return this.symbol;
+    };
+    Card.prototype.IsSameSymbol = function (symbol) {
+        return this.symbol == symbol;
+    };
+    Card.prototype.IsHigher = function (numb) {
+        return this.number >= numb;
+    };
+    Card.Empty = new Card(null, 0);
+    return Card;
+}());
+exports.Card = Card;
