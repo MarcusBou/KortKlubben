ad8cf14508d5af99809c1d0e7e69f0b21b099ed0
MarcusBou <60425915+MarcusBou@users.noreply.github.com>
2022-06-06 20:21:55 +0200

    wee

--- Whist-Version-js/Managers/GameManager.ts
+++ Whist-Version-js/Managers/GameManager.ts
@@ -1,7 +1,11 @@
+import { IGameEngine } from "../Engine/IGameEngine";
+import { WhistGame } from "../Games/WhistGame";
 export class GameManager{
     private id: string;
+    private game: IGameEngine;
     constructor(id){
         this.id = id;
+        this.game = new WhistGame();
     }
 
     public getId(){
