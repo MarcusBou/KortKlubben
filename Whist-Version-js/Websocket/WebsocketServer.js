2398b6b4c002aff51f99d0c730257b2625e0f91f
Sebgineer <sebastian-karlsen@hotmail.com>
2022-06-07 14:21:21 +0200

    WS 2

--- Whist-Version-js/Websocket/WebsocketServer.js
+++ Whist-Version-js/Websocket/WebsocketServer.js
@@ -1,15 +1,16 @@
 "use strict";
 exports.__esModule = true;
-exports.WebSocketServern = void 0;
+exports.WebSocketServer = void 0;
 var ws = require("websocket");
-var http_1 = require("http");
+var http = require("http");
 var WSUser_1 = require("./WSUser");
-var WebSocketServern = /** @class */ (function () {
-    function WebSocketServern() {
+var WebSocketServer = /** @class */ (function () {
+    function WebSocketServer() {
         var _this = this;
         this.activeRooms = new Array();
         this.sessions = new Array();
-        this.server = http_1.createServer(function (request, response) {
+        this.listeners = new Array();
+        this.server = http.createServer(function (request, response) {
             console.log((new Date()) + ': Recieved request for ' + request.url);
             response.writeHead(404);
             response.end();
@@ -21,11 +22,10 @@ var WebSocketServern = /** @class */ (function () {
         this.ws.on('request', function (data) {
             var path = _this.getPathArray(data.resourceURL.path);
             if (path.length != 2) {
-                console.log(path);
                 data.reject(404, "url isnt set currectly");
             }
             else {
-                var roomID_1 = Number.parseInt(path[0]);
+                var roomID_1 = path[0];
                 var username_1 = path[1];
                 var roomFound_1 = false;
                 console.log(roomID_1);
@@ -33,9 +33,21 @@ var WebSocketServern = /** @class */ (function () {
                 _this.activeRooms.forEach(function (room) {
                     if (room == roomID_1 && !roomFound_1) {
                         var session = data.accept();
-                        var user = new WSUser_1.WSUser(session, room, username_1);
-                        _this.sessions.push(user);
+                        var user_1 = new WSUser_1.WSUser(session, room, username_1);
+                        _this.sessions.push(user_1);
                         roomFound_1 = true;
+                        user_1.getSession().on("message", function (message) {
+                            if (message.type === "utf8") {
+                                _this.NotifyOnMessage(message.utf8Data);
+                            }
+                        });
+                        user_1.getSession().on('close', function () {
+                            for (var i = 0; i < _this.sessions.length; i++) {
+                                if (_this.sessions[i] == user_1) {
+                                    _this.sessions.splice(i, 1);
+                                }
+                            }
+                        });
                     }
                 });
                 if (!roomFound_1) {
@@ -43,95 +55,58 @@ var WebSocketServern = /** @class */ (function () {
                 }
             }
         });
-        // this.ws.on('connect', (data) => {
-        //     let connection = data.accept();
-        //     console.log("someone connected");
-        //     console.log(data);
-        //     connection.on('message', function(data) {
-        //         console.log(data);
-        //     })
-        // })
     }
-    WebSocketServern.prototype.originIsAllow = function (origin) {
+    WebSocketServer.prototype.originIsAllow = function (origin) {
         return true;
     };
-    WebSocketServern.prototype.getPathArray = function (path) {
+    WebSocketServer.prototype.getPathArray = function (path) {
         var result = path.split('/');
         if (path[0] == '/') {
             result.splice(0, 1);
         }
         return result;
     };
-    WebSocketServern.prototype.addActiveRoom = function (roomid) {
+    WebSocketServer.prototype.addActiveRoom = function (roomid) {
         this.activeRooms.push(roomid);
     };
-    WebSocketServern.prototype.removeActiveRoom = function (roomID) {
+    WebSocketServer.prototype.removeActiveRoom = function (roomID) {
         for (var i = 0; i < this.activeRooms.length; i++) {
-            this.activeRooms.splice(i, 1);
+            if (this.activeRooms[i] == roomID) {
+                this.activeRooms.splice(i, 1);
+            }
         }
     };
-    return WebSocketServern;
+    WebSocketServer.prototype.addListener = function (listener) {
+        this.listeners.push(listener);
+    };
+    WebSocketServer.prototype.removeListener = function (listener) {
+        for (var i = 0; i < this.listeners.length; i++) {
+            if (this.listeners[i] == listener) {
+                this.listeners.splice(i, 1);
+            }
+        }
+    };
+    WebSocketServer.prototype.NotifyOnMessage = function (message) {
+        this.listeners.forEach(function (listener) {
+            listener.CommandReceived(message);
+        });
+    };
+    WebSocketServer.prototype.broadcastRoom = function (roomID, message) {
+        this.sessions.forEach(function (session) {
+            if (session.getRoomID() == roomID) {
+                session.getSession().sendUTF(message);
+            }
+        });
+    };
+    WebSocketServer.prototype.broadcastUsername = function (username, message) {
+        this.sessions.forEach(function (session) {
+            if (session.getUsername() == username) {
+                session.getSession().sendUTF(message);
+            }
+        });
+    };
+    return WebSocketServer;
 }());
-exports.WebSocketServern = WebSocketServern;
-// var WebSocketServer = require('websocket').server;
-// //var http = require('http');
-// var connections = [];
-// // var server = new http.createServer(function (request, response) {
-// //     console.log((new Date()) + ': Recieved request for ' + request.url);
-// //     response.writeHead(404);
-// //     response.end();
-// // });
-// server.listen(5000, function() {
-//     console.log((new Date()) + ': Server port is 5000');
-// });
-// var wsServer = new WebSocketServer({
-//     httpServer: server,
-//     autoAcceptConnections: false,
-// });
-// function originIsAllowed(origin) {
-//     console.log(origin)
-//     // put logic here to detect whether the specified origin is allowed.
-//     return true;
-// }
-// wsServer.on('open', function(data) {
-//     console.log("server open");
-// })
-// wsServer.on('request', function(request) {
-//     if (!originIsAllowed(request.origin)) {
-//         // Make sure we only accept requests from an allowed origin
-//         request.reject();
-//         console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-//         return;
-//     }
-//     var connection = request.accept();
-//     connections.push(connection);
-//     console.log(connections);
-//     connection.on('message', function(message) {
-//         if (message.type === 'utf8') {
-//             console.log('Message Received: ' + message.utf8Data)
-//             connections.forEach(con => {
-//                 con.sendUTF('Server Received: '+message.utf8Data);
-//             });
-//         } else if (message.binaryData) {
-//             console.log('Message Received: ' + message.binaryData);
-//             connection.sendBytes(message.binaryData);
-//         }
-//     });
-//     connection.on('close', function(reasonCode, description) {
-//         for( var i = 0; i < connections.length; i++){ 
-//             if ( connections[i] === connection) { 
-//                 connections.splice(i, 1); 
-//             }
-//         }
-//         console.log(connections);
-//         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-//     });
-// });
-// wsServer.on('connection', function(requset) {
-//     var connection = request.accept();
-//     console.log("someone connected");
-//     console.log(requset);
-//     connection.on('message', function(data) {
-//         console.log(data);
-//     })
-// });
+exports.WebSocketServer = WebSocketServer;
+// let kage = new WebSocketServer();
+// kage.addActiveRoom("2002");
--- Whist-Version-js/Websocket/WebsocketServer.ts
+++ Whist-Version-js/Websocket/WebsocketServer.ts
@@ -1,16 +1,19 @@
 import * as ws from "websocket";
-import http from 'http'
+import * as http from 'http'
 import { WSUser } from "./WSUser";
+import { WSlistener } from "../Listeners/WsListener";
 
-export class WebSocketServern {
+export class WebSocketServer {
     private server: http.Server;
     private ws: ws.server;
     private activeRooms: Array<string>;
     private sessions: Array<WSUser>;
+    private listeners: Array<WSlistener>;
 
     constructor() {
         this.activeRooms = new Array<string>();
         this.sessions = new Array<WSUser>();
+        this.listeners = new Array<WSlistener>();
 
         this.server = http.createServer(function (request, response) {
             console.log((new Date()) + ': Recieved request for ' + request.url);
@@ -45,22 +48,25 @@ export class WebSocketServern {
                         let user: WSUser = new WSUser(session, room, username);
                         this.sessions.push(user);
                         roomFound = true;
+                        user.getSession().on("message", (message: ws.Message) => {
+                            if (message.type === "utf8") {
+                                this.NotifyOnMessage(message.utf8Data);
+                            }
+                        });
+                        user.getSession().on('close', () => {
+                            for (let i = 0; i < this.sessions.length; i++) {
+                                if (this.sessions[i] == user) {
+                                    this.sessions.splice(i, 1);
+                                }
+                            }
+                        });
                     }
                 });
-                
                 if (!roomFound) {
                     data.reject();
                 }
             }
         });
-        // this.ws.on('connect', (data) => {
-        //     let connection = data.accept();
-        //     console.log("someone connected");
-        //     console.log(data);
-        //     connection.on('message', function(data) {
-        //         console.log(data);
-        //     })
-        // })
     }
 
     private originIsAllow(origin): boolean {
@@ -81,79 +87,46 @@ export class WebSocketServern {
 
     public removeActiveRoom(roomID: string): void {
         for (let i = 0; i < this.activeRooms.length; i++) {
-            this.activeRooms.splice(i, 1);
+            if (this.activeRooms[i] == roomID) {
+                this.activeRooms.splice(i, 1);
+            }
         }
+    }
+
+    public addListener(listener: WSlistener) {
+        this.listeners.push(listener);
+    }
+    
+    public removeListener(listener: WSlistener) {
+        for (let i = 0; i < this.listeners.length; i++) {
+            if (this.listeners[i] == listener) {
+                this.listeners.splice(i, 1);
+            }
+        }
+    }
+    
+    private NotifyOnMessage(message: string) {
+        this.listeners.forEach(listener => {
+            listener.CommandReceived(message);
+        });
+    }
+
+    public broadcastRoom(roomID: string, message: string): void {
+        this.sessions.forEach(session => {
+            if (session.getRoomID() == roomID) {
+                session.getSession().sendUTF(message);
+            }
+        });
+    }
+
+    public broadcastUsername(username: string, message: string) {
+        this.sessions.forEach(session => {
+            if (session.getUsername() == username) {
+                session.getSession().sendUTF(message);
+            }
+        });
     }
 }
 
-// var WebSocketServer = require('websocket').server;
-// //var http = require('http');
-// var connections = [];
-
-// // var server = new http.createServer(function (request, response) {
-// //     console.log((new Date()) + ': Recieved request for ' + request.url);
-// //     response.writeHead(404);
-// //     response.end();
-// // });
-
-// server.listen(5000, function() {
-//     console.log((new Date()) + ': Server port is 5000');
-// });
-
-// var wsServer = new WebSocketServer({
-//     httpServer: server,
-//     autoAcceptConnections: false,
-// });
-
-// function originIsAllowed(origin) {
-//     console.log(origin)
-//     // put logic here to detect whether the specified origin is allowed.
-//     return true;
-// }
-
-// wsServer.on('open', function(data) {
-//     console.log("server open");
-// })
-
-// wsServer.on('request', function(request) {
-//     if (!originIsAllowed(request.origin)) {
-//         // Make sure we only accept requests from an allowed origin
-//         request.reject();
-//         console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-//         return;
-//     }
-
-//     var connection = request.accept();
-//     connections.push(connection);
-//     console.log(connections);
-//     connection.on('message', function(message) {
-//         if (message.type === 'utf8') {
-//             console.log('Message Received: ' + message.utf8Data)
-//             connections.forEach(con => {
-//                 con.sendUTF('Server Received: '+message.utf8Data);
-//             });
-//         } else if (message.binaryData) {
-//             console.log('Message Received: ' + message.binaryData);
-//             connection.sendBytes(message.binaryData);
-//         }
-//     });
-//     connection.on('close', function(reasonCode, description) {
-//         for( var i = 0; i < connections.length; i++){ 
-//             if ( connections[i] === connection) { 
-//                 connections.splice(i, 1); 
-//             }
-//         }
-//         console.log(connections);
-//         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-//     });
-// });
-
-
-// wsServer.on('connection', function(requset) {
-//     var connection = request.accept();
-//     console.log("someone connected");
-//     console.log(requset);
-//     connection.on('message', function(data) {
-//         console.log(data);
-//     })
-// });
+// let kage = new WebSocketServer();
+// kage.addActiveRoom("2002");
